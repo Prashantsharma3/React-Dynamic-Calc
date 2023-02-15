370ffb253370f79151285a8cfd22d8f70c8dcabc
Prashant Sharma <prashantsharmaxxx1@gmail.com>
2023-02-15 20:46:37 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,30 +1,30 @@
 import React,{useState,useEffect} from 'react'
 import '../styles/App.css';
 function App() {
-  const [input1, setInput1] = useState(0);
-  const [input2, setInput2] = useState(0);
-  const [result, setResult] = useState(0);
+const [num1, setNum1] = useState(0);
+const [num2, setNum2] = useState(0);
+const [result, setResult] = useState(0);
 
-  const handleInput1 = (e) => {
-    setInput1(Number(e.target.value));
-  };
+const handleNum1Change = (e) => {
+setNum1(e.target.value);
+};
 
-  const handleInput2 = (e) => {
-    setInput2(Number(e.target.value));
-  };
+const handleNum2Change = (e) => {
+setNum2(e.target.value);
+};
 
-  const handleAddition = () => {
-    setResult(input1 + input2);
-  };
+const handleAddition = () => {
+setResult(Number(num1) + Number(num2));
+};
 
-  return (
-    <div>
-      <input type="number" id="input1" value={input1} onChange={handleInput1} />
-      <input type="number" id="input2" value={input2} onChange={handleInput2} />
-      <button onClick={handleAddition}>Add</button>
-      <p id="result">{result}</p>
-    </div>
-  );
+return (
+<div>
+<input id="input1" type="number" value={num1} onChange={handleNum1Change} />
+<input id="input2" type="number" value={num2} onChange={handleNum2Change} />
+<button onClick={handleAddition}>Add</button>
+<p id="result">{result}</p>
+</div>
+);
 }
 
 export default App;
